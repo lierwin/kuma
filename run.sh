97909a14bda19a606dc975e4351990ea6224d334
lierwin <58074357+lierwin@users.noreply.github.com>
2025-10-22 14:10:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-set -e
 
 echo "trying to restore the database (if it exists)..."
-/usr/local/bin/litestream restore -v -if-replica-exists /app/data/kuma.db
+litestream restore -v -if-replica-exists /app/data/kuma.db
 
 echo "starting replication and the application..."
-exec /usr/local/bin/litestream replicate
+litestream replicate
